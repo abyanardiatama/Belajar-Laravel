028ce0bc94f0469717c17354b56a7f1be158634c
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-05-18 07:52:27 +0700

    percobaan conflict 2

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,4 +4,8 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h2">Selamat datang kembali, {{ auth()->user()->name }}</h1>
 </div>
+
+<div>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, facilis.</p>
+</div>
 @endsection
